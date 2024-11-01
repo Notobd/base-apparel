a41e91891b022e96942d5a281506a71bbe6699a5
notobd <notobd4x4@gmail.com>
2024-10-31 17:59:19 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+const form = document.querySelector('#input-form');
+console.log(form);
+const emailInput = document.querySelector('#email');
+console.log(emailInput);
+const subBtn = document.querySelector('.submit');
+console.log(subBtn);
+const message = document.querySelector('.error');
+console.log(message);
+
+// created a regualr expression to validate email adding the .test method to the regex variable and pass in the emailinput value as argument
+//emailReg.test(emailInput.value)
+const emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+const validateMail = function () {
+  let mailValue = emailInput.value;
+  if (mailValue === '') {
+    message.textContent = 'Input a mail';
+  } else if (!emailReg.test(mailValue)) {
+    message.textContent = 'Provide valid mail';
+  } else {
+    message.textContent = 'sucess';
+  }
+};
+
+//attched an event handler to the submit button to listen for events and prevent medtod to prevent form from submiting on click and called the validate function here o check mail input and also set the input field back to empty after submiting
+const submit = e => {
+  e.preventDefault();
+  validateMail();
+  emailInput.value = '';
+};
+subBtn.addEventListener('click', submit);
+
+//extra functionality to make the enter button act as the submit button added the event listener to the entire document thats why its not attached to any defined variable in the global scope
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    console.log(e);
+    e.preventDefault();
+    validateMail();
+    emailInput.value = '';
+  }
+});
